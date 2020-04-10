3f72cb415d3984846820b99831b6e94449a2bcf5
yildizadem <ademyildiz9258@gmail.com>
2020-04-10 16:00:42 +0300

    Updates

---
+++ precache-manifest.8e4847c593ef6e4c099aa9d4c707b0aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1456d672a82c81692456db2900057adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f935c873fe1fa65ebc",
+    "url": "/static/css/main.91c00ca6.chunk.css"
+  },
+  {
+    "revision": "ac1994b17e3742548e7a",
+    "url": "/static/js/2.c46b201a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c46b201a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f935c873fe1fa65ebc",
+    "url": "/static/js/main.188f032e.chunk.js"
+  },
+  {
+    "revision": "e0da735e5edb4ded2442",
+    "url": "/static/js/runtime-main.e701eb16.js"
+  },
+  {
+    "revision": "ef070b68750187854b533196b635f09d",
+    "url": "/static/media/ibb-logo-wide.ef070b68.svg"
+  },
+  {
+    "revision": "39bf48cc867b24de254d65ab33b6a5f9",
+    "url": "/static/media/ibb-logo.39bf48cc.svg"
+  },
+  {
+    "revision": "369f43913bb0245dea05c4e5cab071a0",
+    "url": "/static/media/keyboard-arrow-down-24-px.369f4391.svg"
+  },
+  {
+    "revision": "d3248c38e84a4eee89455743ea20398a",
+    "url": "/static/media/notifications-active-24-px.d3248c38.svg"
+  },
+  {
+    "revision": "2f56cbc760ca58a18d122bf397817196",
+    "url": "/static/media/ruzgarli-48-px.2f56cbc7.svg"
+  },
+  {
+    "revision": "4dfb8bbacbc991885f3096cd68a2297e",
+    "url": "/static/media/searchIcon.4dfb8bba.svg"
+  }
+]);
