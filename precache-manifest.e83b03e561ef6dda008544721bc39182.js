68d60ffa2d88c63dd49110486810c21c546545df
yildizadem <ademyildiz9258@gmail.com>
2020-04-10 16:04:07 +0300

    Updates

---
+++ precache-manifest.e83b03e561ef6dda008544721bc39182.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f0d2f5d8d101a8100ac2e3201318f2",
+    "url": "/platform/index.html"
+  },
+  {
+    "revision": "e00d7af08c1806befb04",
+    "url": "/platform/static/css/main.1c2bfa0b.chunk.css"
+  },
+  {
+    "revision": "36883c1a4ebacddf0bb9",
+    "url": "/platform/static/js/2.edd12b74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/platform/static/js/2.edd12b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00d7af08c1806befb04",
+    "url": "/platform/static/js/main.f14e6dab.chunk.js"
+  },
+  {
+    "revision": "70468b2ea86ba300a501",
+    "url": "/platform/static/js/runtime-main.0d294fe5.js"
+  },
+  {
+    "revision": "08ae7c346cf868d13274281d2a7cab43",
+    "url": "/platform/static/media/acik-48px.08ae7c34.svg"
+  },
+  {
+    "revision": "23430d63860356c7f4586f4cfda90e7c",
+    "url": "/platform/static/media/cok-bulutlu--48px.23430d63.svg"
+  },
+  {
+    "revision": "af3a9df21053047430484f2bc4611598",
+    "url": "/platform/static/media/hafif-kar-yagisli-48px.af3a9df2.svg"
+  },
+  {
+    "revision": "ef070b68750187854b533196b635f09d",
+    "url": "/platform/static/media/ibb-logo-wide.ef070b68.svg"
+  },
+  {
+    "revision": "39bf48cc867b24de254d65ab33b6a5f9",
+    "url": "/platform/static/media/ibb-logo.39bf48cc.svg"
+  },
+  {
+    "revision": "2810bcfa7b9cbb6eadbd630a6bae1a90",
+    "url": "/platform/static/media/parcali-bulutu-48px.2810bcfa.svg"
+  },
+  {
+    "revision": "2f56cbc760ca58a18d122bf397817196",
+    "url": "/platform/static/media/ruzgarli-48-px.2f56cbc7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88740e24e67c684b677a562d694038a7.js"
+  "/platform/precache-manifest.e83b03e561ef6dda008544721bc39182.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/platform/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
